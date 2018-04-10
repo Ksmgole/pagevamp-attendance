56ec52961a81496af96f6f5b14c83f6ac9822434
Ksmgole <kusumgole67@gmail.com>
2018-04-10 06:41:37 +0545

    all duplicate code deleted

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,18 +14,13 @@ class UserController extends Controller
         return view ('auth.login');
 
     }
-<<<<<<< HEAD
     public function adminDashboard (){
-=======
-    public function admin_dashboard (){
->>>>>>> 93ca9ebc8d40186bd7c5a43e27cb730f0f8d4add
         return view ('attendance.admin');
     }
 
     public function memberDashboard (){
         return view ('layout.member');
     }
-<<<<<<< HEAD
     public function checkLogin(Request $request){
     if(Auth::attempt($request->only(['$email','$password']))){
         $user= Auth::user();
@@ -34,19 +29,6 @@ class UserController extends Controller
         }
         return redirect('/member');
     }
-=======
-
-    public function postLogin(Request $request){
-
-        if(Auth::attempt($request->only(['email','password']))) {
-            $user = Auth::user();
-            if($user->is_admin) {
-                return redirect('/admin');
-            }
-
-            return redirect('member');
-        }
->>>>>>> 93ca9ebc8d40186bd7c5a43e27cb730f0f8d4add
 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-<<<<<<< HEAD
 Route::get('/', 'UserController@loginPage');
 Route::get('/admin', 'UserController@adminDashboard')->name('admin.dashboard');
 Route::get('/member', 'UserController@memberDashboard');
 Route::post('/login','UserController@checkLogin')->name('login.post');
-=======
-Route::get('/login', 'UserController@loginPage')->name('login.get');
-Route::get('/admin', 'UserController@admin_dashboard');
-Route::get('/member', 'UserController@member_dashboard');
-Route::post('','UserController@checklogin');
-
-Route::post('login','UserController@postLogin')->name('login.post');
->>>>>>> 93ca9ebc8d40186bd7c5a43e27cb730f0f8d4add
